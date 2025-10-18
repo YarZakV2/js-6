7ab991227a0a0fef72c32979667ea48e25c6fdca
YarZakV2 <yarik116117.com@gmail.com>
2025-10-18 20:20:01 +0300

    .

--- dist/js-6.31b563d9.js
+++ dist/js-6.31b563d9.js
@@ -207,11 +207,11 @@
       });
     }
   }
-})({"6WpVq":[function(require,module,exports,__globalThis) {
+})({"5j6Kf":[function(require,module,exports,__globalThis) {
 var global = arguments[3];
 var HMR_HOST = null;
 var HMR_PORT = null;
-var HMR_SERVER_PORT = 62300;
+var HMR_SERVER_PORT = 1234;
 var HMR_SECURE = false;
 var HMR_ENV_HASH = "439701173a9199ea";
 var HMR_USE_SSE = false;
@@ -764,6 +764,6 @@ parcelHelpers.defineInteropFlag(exports);
 parcelHelpers.export(exports, "message", ()=>message);
 const message = "Parcel";
 
-},{"@parcel/transformer-js/src/esmodule-helpers.js":"jnFvT"}]},["6WpVq","a0t4e"], "a0t4e", "parcelRequire2620", {})
+},{"@parcel/transformer-js/src/esmodule-helpers.js":"jnFvT"}]},["5j6Kf","a0t4e"], "a0t4e", "parcelRequire2620", {})
 
 //# sourceMappingURL=js-6.31b563d9.js.map
